3e6d6589ef5c3c2b80964f9366081999b4d8d3be
Angristan <stanislas.lange@openmailbox.org>
2015-07-16 22:30:55 +0200

    Optimisation grep/awk

--- ram.sh
+++ ram.sh
@@ -1,5 +1,5 @@
-totalkb=`cat /proc/meminfo | grep MemTotal | awk '{print $2}'`
-freekb=`cat /proc/meminfo | grep MemFree | awk '{print $2}'`
+totalkb=`grep MemTotal /proc/meminfo | awk '{print $2}'`
+freekb=`grep MemFree /proc/meminfo | awk '{print $2}'`
 let "totalmb = $totalkb / 1000"
 let "freemb = $freekb / 1000"
 python /home/pi/TweetBot/tweet.py "J'ai "$freemb" Mo de RAM de libre sur "$totalmb"."
