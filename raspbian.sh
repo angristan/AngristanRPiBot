d585981ad8ce1b97fa626e31270db4c7f94d841b
Angristan <stanislas.lange@openmailbox.org>
2015-08-16 20:40:41 +0200

    Update raspbian.sh

--- raspbian.sh
+++ raspbian.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 rpiv=`uname -a | awk '{print $4}'`
-python /home/pi/TweetBot/tweet.py "Ma version de Raspbian est la "$rpiv"
+python /home/pi/TweetBot/tweet.py "Ma version de Raspbian est la $rpiv"
