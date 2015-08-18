0ceb44ebb6fa8c6bb46f730294b2bb8903cad78b
Angristan <stanislas.lange@openmailbox.org>
2015-08-18 18:50:27 +0200

    Update linux.sh

--- linux.sh
+++ linux.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-kernel=`uname -a | awk '{print $3}'`
+kernel=$(uname -a | awk '{print $3}')
 echo $kernel > /home/pi/TweetBot/kernel.txt
 sed -i 's|+||' /home/pi/TweetBot/kernel.txt
-kernel=`cat /home/pi/TweetBot/kernel.txt?`
+kernel=$(cat /home/pi/TweetBot/kernel.txt)
 python /home/pi/TweetBot/tweet.py "Ma version de Linux est la "$kernel
