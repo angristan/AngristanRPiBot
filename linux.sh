9bb4ddc5e50de3ab8d8524a16f7d23603ff0ab24
Angristan <stanislas.lange@openmailbox.org>
2016-01-05 18:31:19 +0100

    Update linux.sh

--- linux.sh
+++ linux.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 kernel=$(uname -a | awk '{print $3}')
-echo $kernel > /home/pi/TweetBot/kernel.txt
-sed -i 's|+||' /home/pi/TweetBot/kernel.txt
-kernel=$(cat /home/pi/TweetBot/kernel.txt)
-rm /home/pi/TweetBot/kernel.txt
-python /home/pi/TweetBot/tweet.py "Ma version de Linux est la "$kernel
+echo $kernel > /home/pi/rpibot/kernel.txt
+sed -i 's|+||' /home/pi/rpibot/kernel.txt
+kernel=$(cat /home/pi/rpibot/kernel.txt)
+rm /home/pi/rpibot/kernel.txt
+python /home/pi/rpibot/tweet.py "Ma version de Linux est la "$kernel
