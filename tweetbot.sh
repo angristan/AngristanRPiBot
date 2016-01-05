dcfecc09d5ed83a6dca370f97165e78c25425e15
Angristan <stanislas.lange@openmailbox.org>
2016-01-05 18:30:21 +0100

    Update tweetbot.sh

--- tweetbot.sh
+++ tweetbot.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
-if [ -e /home/pi/TweetBot/tweetcount.txt ]
+if [ -e /home/pi/rpibot/tweetcount.txt ]
 then
-	tweetcount=$(cat /home/pi/TweetBot/tweetcount.txt)
+	tweetcount=`cat /home/pi/rpibot/tweetcount.txt`
 else
-	echo "1" > /home/pi/TweetBot/tweetcount.txt
-	tweetcount=$(cat /home/pi/TweetBot/tweetcount.txt)
+	echo "1" > /home/pi/rpibot/tweetcount.txt
+	tweetcount=`cat /home/pi/rpibot/tweetcount.txt`
 fi
 if [[ $tweetcount == "1" ]]
 then
-	bash /home/pi/TweetBot/ram.sh
-	echo "2" > /home/pi/TweetBot/tweetcount.txt
+	bash /home/pi/rpibot/ram.sh
+	echo "2" > /home/pi/rpibot/tweetcount.txt
 	exit
 elif [[ $tweetcount == "2" ]]
 then
-	bash /home/pi/TweetBot/load.sh
-	echo "3" > /home/pi/TweetBot/tweetcount.txt
+	bash /home/pi/rpibot/load.sh
+	echo "3" > /home/pi/rpibot/tweetcount.txt
 	exit
 elif [[ $tweetcount == "3" ]]
 then
-	python /home/pi/TweetBot/temp.py
-	echo "4" > /home/pi/TweetBot/tweetcount.txt
+	python /home/pi/rpibot/temp.py
+	echo "4" > /home/pi/rpibot/tweetcount.txt
 	exit
 elif [[ $tweetcount == "4" ]]
 then
-	python /home/pi/TweetBot/uptime.py
-	echo "1" > /home/pi/TweetBot/tweetcount.txt
+	python /home/pi/rpibot/uptime.py
+	echo "1" > /home/pi/rpibot/tweetcount.txt
 	exit
 fi
