904d05c8aeccdbd4c4424ba94591f03d82470b89
Angristan <stanislas.lange@openmailbox.org>
2016-01-05 18:29:53 +0100

    Update ram.sh

--- ram.sh
+++ ram.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-totalkb=$(grep MemTotal /proc/meminfo | awk '{print $2}')
-#freekb=$(grep MemFree /proc/meminfo | awk '{print $2}')
-freekb=$(grep MemAvailable /proc/meminfo | awk '{print $2}')
+totalkb=`grep MemTotal /proc/meminfo | awk '{print $2}'`
+freekb=`grep MemAvailable /proc/meminfo | awk '{print $2}'`
 let "totalmb = $totalkb / 1000"
 let "freemb = $freekb / 1000"
 let "usedmb = $totalmb - $freemb"
 let "usedkb = $totalkb - $freekb"
 let "totalkbpc = $totalkb / 100"
 let "percent = $usedkb / $totalkbpc"
-#python /home/pi/TweetBot/tweet.py "J'ai "$freemb" Mo de RAM de libre sur "$totalmb"."
-python /home/pi/TweetBot/tweet.py "J'utilise "$usedmb" Mo de RAM sur "$totalmb" Mo, soit "$percent"%."
+#let "percent = $percent * 100"
+#python /home/pi/rpibot/tweet.py "J'ai "$freemb" Mo de RAM de libre sur "$totalmb"."
+python /home/pi/rpibot/tweet.py "J'utilise "$usedmb" Mo de RAM sur "$totalmb" Mo, soit "$percent"%."
